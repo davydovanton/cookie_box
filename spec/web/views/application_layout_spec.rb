3762811693468e3a3c617466c5cf7dfddaa72251
Anton Davydov <antondavydov.o@gmail.com>
2017-12-25 21:29:45 +0300

    Initialize project

---
+++ spec/web/views/application_layout_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Web::Views::ApplicationLayout, type: :view do
+  let(:layout)   { Web::Views::ApplicationLayout.new(template, {}) }
+  let(:rendered) { layout.render }
+  let(:template) { Hanami::View::Template.new('apps/web/templates/application.html.slim') }
+
+  it 'contains application name' do
+    expect(rendered).to include('Web')
+  end
+end
