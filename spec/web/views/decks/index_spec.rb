1c327f1eb52defdfe3cd1c3a796f76271e271a02
Anton Davydov <antondavydov.o@gmail.com>
2017-12-31 16:50:16 +0300

    Add specs to new deck form

--- spec/web/views/decks/index_spec.rb
+++ spec/web/views/decks/index_spec.rb
@@ -1,10 +1,11 @@
 RSpec.describe Web::Views::Decks::Index, type: :view do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { Hash[params: {}] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/decks/index.html.slim') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    expect(view.foo).to eq exposures.fetch(:foo)
+  context '#form' do
+    it { expect(view.form).to have_method(:post) }
+    it { expect(view.form).to have_action('/decks') }
   end
 end
