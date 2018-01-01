d7256293c328ce2ba461e2a8396651fef8f5ad63
Anton Davydov <antondavydov.o@gmail.com>
2018-01-01 17:32:18 +0300

    Add logic to publish endpoint

--- apps/web/controllers/published_decks/create.rb
+++ apps/web/controllers/published_decks/create.rb
@@ -1,9 +1,14 @@
 module Web::Controllers::PublishedDecks
   class Create
     include Web::Action
+    include Import[operation: 'decks.operations.publish']
+
+    before :authenticate!
 
     def call(params)
-      redirect_to routes.root_path
+      # TODO: security issue. Anyone cau publish deck
+      operation.call(params[:deck][:id])
+      redirect_to routes.decks_path
     end
   end
 end
--- spec/web/controllers/published_decks/create_spec.rb
+++ spec/web/controllers/published_decks/create_spec.rb
@@ -1,6 +1,24 @@
 RSpec.describe Web::Controllers::PublishedDecks::Create, type: :action do
-  let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  include Dry::Monads::Either::Mixin
 
-  it { expect(action.call(params)).to redirect_to('/') }
+  let(:mock_operation) { Mock::SuccessListOperation.new }
+  let(:action) { described_class.new(operation: mock_operation) }
+  let(:params)  { { 'rack.session' => session, deck: { id: 1 } } }
+
+  context 'when account login' do
+    let(:session) { { account: Account.new(id: 1) } }
+
+    it { expect(action.call(params)).to redirect_to('/decks') }
+
+    it 'calls operation with current account id' do
+      expect(mock_operation).to receive(:call).with(1)
+      action.call(params)
+    end
+  end
+
+  context 'when account not login' do
+    let(:session) { {} }
+
+    it { expect(action.call(params)).to redirect_to('/') }
+  end
 end
