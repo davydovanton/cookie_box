50e56e7f05d882e7301f8855255a2afc52811aa3
Anton Davydov <antondavydov.o@gmail.com>
2018-01-06 02:12:58 +0300

    Add missing repo specs

--- spec/cookie_box/repositories/repository_repository_spec.rb
+++ spec/cookie_box/repositories/repository_repository_spec.rb
@@ -1,3 +1,10 @@
 RSpec.describe RepositoryRepository, type: :repository do
-  # place your tests here
+  let(:repo) { described_class.new }
+
+  describe '#find_by_name' do
+    before { Fabricate.create(:repository, full_name: 'hanami/hanami') }
+
+    it { expect(repo.find_by_name('hanami/events')).to be_nil }
+    it { expect(repo.find_by_name('hanami/hanami')).to_not be_nil }
+  end
 end
