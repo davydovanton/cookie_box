9886a6d6054c225ea46f857a55df083896a2609c
Anton Davydov <antondavydov.o@gmail.com>
2018-01-08 14:06:45 +0300

    Display issues on deck page

--- apps/web/controllers/decks/show.rb
+++ apps/web/controllers/decks/show.rb
@@ -1,12 +1,13 @@
 module Web::Controllers::Decks
   class Show
     include Web::Action
-    include Import[operation: 'decks.operations.show']
+    include Import[operation: 'decks.operations.show', get_issues: 'decks.operations.all_issues']
 
-    expose :deck
+    expose :deck, :issues
 
     def call(params)
       operation.call(params[:id]).fmap { |value| @deck = value }
+      get_issues.call(deck_id: params[:id]).fmap { |value| @issues = value }
       status 404, 'Not found' if deck_protected?
     end
 
