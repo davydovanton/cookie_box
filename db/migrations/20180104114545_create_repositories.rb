4161d54ab1e12cb985bad9cdf1cc67190b950632
Anton Davydov <antondavydov.o@gmail.com>
2018-01-23 00:15:08 +0300

    Create webhook_owner_id column for repositories

--- db/migrations/20180104114545_create_repositories.rb
+++ db/migrations/20180104114545_create_repositories.rb
@@ -3,10 +3,11 @@ Hanami::Model.migration do
     create_table :repositories do
       primary_key :id
 
-      column :full_name,   String
-      column :description, String
-      column :html_url,    String
-      column :topics,      String
+      column :full_name,        String
+      column :description,      String
+      column :html_url,         String
+      column :topics,           String
+      column :webhook_owner_id, Integer, default: nil
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
