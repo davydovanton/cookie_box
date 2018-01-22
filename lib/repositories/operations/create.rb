d52f30015ad9ec8dae1f966c3e2eece746ae8ea4
Anton Davydov <antondavydov.o@gmail.com>
2018-01-22 22:36:56 +0300

    Drop dry-tr because it's broke operation (random steps call)

--- lib/repositories/operations/create.rb
+++ lib/repositories/operations/create.rb
@@ -3,22 +3,17 @@ require 'dry/transaction'
 module Repositories
   module Operations
     class Create < Core::Operation
-      include Dry::Transaction
       include Import['repositories.deck_repo', 'repositories.libs.get_or_create_repo']
-
-      step :validate
-      step :persist
 
       VALIDATOR = Dry::Validation.Form do
         required(:deck_id).filled(:int?)
         required(:repo_name).filled(:str?)
       end
 
-      def validate(payload)
-        VALIDATOR.call(payload).to_either
-      end
+      def call(payload)
+        result = VALIDATOR.call(payload).to_either
+        return result if result.left?
 
-      def persist(payload)
         get_or_create_repo.call(payload[:repo_name]).fmap do |repo|
           deck_repo.create(deck_id: payload[:deck_id], repository_id: repo.id)
         end
--- lib/repositories/operations/delete.rb
+++ lib/repositories/operations/delete.rb
@@ -3,22 +3,17 @@ require 'dry/transaction'
 module Repositories
   module Operations
     class Delete < Core::Operation
-      include Dry::Transaction
       include Import['repositories.deck_repo']
-
-      step :validate
-      step :persist
 
       VALIDATOR = Dry::Validation.Form do
         required(:deck_id).filled(:int?)
         required(:repository_id).filled(:int?)
       end
 
-      def validate(payload)
-        VALIDATOR.call(payload).to_either
-      end
+      def call(payload)
+        result = VALIDATOR.call(payload).to_either
+        return result if result.left?
 
-      def persist(payload)
         Right(deck_repo.delete_from_deck(payload[:deck_id], payload[:repository_id]))
       end
     end
