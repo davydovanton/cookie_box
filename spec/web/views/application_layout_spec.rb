d3a29460bd300c9b7ea6a0de7a1b2a2d5b239dac
Anton Davydov <antondavydov.o@gmail.com>
2018-01-23 00:51:43 +0300

    Fix specs

--- spec/web/views/application_layout_spec.rb
+++ spec/web/views/application_layout_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Web::Views::ApplicationLayout, type: :view do
   let(:layout)   { Web::Views::ApplicationLayout.new(template, {}) }
   let(:rendered) { layout.render }
   let(:template) { Hanami::View::Template.new('apps/web/templates/application.html.slim') }
-
-  it 'contains application name' do
-    expect(rendered).to include('Web')
-  end
 end
