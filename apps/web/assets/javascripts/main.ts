3c6b91454d9c194d3b9268b528e376c12909987f
Anton Davydov <antondavydov.o@gmail.com>
2018-01-30 00:27:10 +0300

    Toggle repositories on show page

--- apps/web/assets/javascripts/main.ts
+++ apps/web/assets/javascripts/main.ts
@@ -1,3 +1,7 @@
 require("!style-loader!css-loader!sass-loader!../stylesheets/main.scss");
 
-console.log('hello from webpack and ts')
+console.log('hello from webpack and ts');
+
+document.querySelector('#repository-toggle').addEventListener('click', function() {
+  document.querySelector('.repositories').classList.toggle('hide');
+});
