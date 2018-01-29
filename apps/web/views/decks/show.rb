3d1a823964e17748ded4a9ab23686cc0b3e12e5f
Anton Davydov <antondavydov.o@gmail.com>
2018-01-30 00:09:24 +0300

    Update repository layout (looks like a card now)

--- apps/web/views/decks/show.rb
+++ apps/web/views/decks/show.rb
@@ -8,10 +8,10 @@ module Web::Views::Decks
 
     def delete_repository_button(repository_id)
       html.form(action: "/repositories/#{repository_id}", method: "POST") do
-        input(type: "hidden", name: "_method", value: "DELETE")
-        input(type: "hidden", name: "deck_id", value: deck.id)
+        input type: "hidden", name: "_method", value: "DELETE"
+        input type: "hidden", name: "deck_id", value: deck.id
 
-        input(type: "submit", value: "Delete Repository")
+        input type: "submit", value: "✕"
       end
     end
 
