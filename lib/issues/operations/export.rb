35bde8a5b6fc17eaf76af30146f78f870bebc4e2
Anton Davydov <antondavydov.o@gmail.com>
2018-02-03 14:33:44 +0300

    Use do notation in export issues operation

--- lib/issues/operations/export.rb
+++ lib/issues/operations/export.rb
@@ -1,18 +1,29 @@
 module Issues
   module Operations
     class Export < Core::Operation
+      include Dry::Monads::Do.for(:call)
+
       include Import[
         'repositories.repository', 'repositories.issue',
         'issues.libs.github_list', :logger
       ]
 
       def call(repository_id)
-        repo_entity = repository.find(repository_id)
-        return Left(:not_found) unless repo_entity
+        entity = yield find(repository_id)
+        issues = yield github_list.call(entity)
 
-        github_list.call(repo_entity).fmap do |issues_payload|
-          issues_payload.each { |payload| create_issue(repository_id: repository_id, **payload) }
-        end
+        Right(persist_issues(issues, repository_id))
+      end
+
+    private
+
+      def find(id)
+        entity = repository.find(id)
+        entity ? Right(entity) : Left(:not_found)
+      end
+
+      def persist_issues(issues, repository_id)
+        issues.each { |payload| create_issue(repository_id: repository_id, **payload) }
       end
 
       def create_issue(payload)
