0491181450f04c5da5eea358ad4b9294adb3737f
Anton Davydov <antondavydov.o@gmail.com>
2018-02-03 14:47:33 +0300

    Improve specs coverage

--- spec/issues/workers/export_spec.rb
+++ spec/issues/workers/export_spec.rb
@@ -2,12 +2,31 @@ RSpec.describe Issues::Workers::Export do
   include Dry::Monads::Either::Mixin
 
   let(:operation) { -> (arg) { Right(arg) } }
-  let(:worker) { described_class.new(operation: operation) }
+  let(:logger) { double(:logger, info: nil) }
+  let(:worker) { described_class.new(logger: logger, operation: operation) }
 
   subject { worker.perform(1) }
 
   it 'calls operation' do
     expect(operation).to receive(:call).with(1).and_return(Right(1))
     subject
+  end
+
+  context 'when operation returns success result' do
+    let(:operation) { -> (arg) { Right(arg) } }
+
+    it 'does not log something' do
+      expect(logger).to_not receive(:info)
+      subject
+    end
+  end
+
+  context 'when operation returns failed result' do
+    let(:operation) { -> (arg) { Left(arg) } }
+
+    it 'does not log something' do
+      expect(logger).to receive(:info)
+      subject
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,14 @@ SimpleCov.start do
   add_filter "/spec/"
   add_filter "/config/"
   add_filter "/db/"
+  add_filter "/system/"
+
+  %w[web].each do |app|
+    add_filter "/apps/#{app}/application"
+    add_filter "/apps/#{app}/templates"
+    add_filter "/apps/#{app}/config"
+    add_filter "/apps/#{app}/assrts"
+  end
 end
 
 require 'fabrication'
