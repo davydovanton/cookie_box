9408adbe6676668fedaeb453df2c5a7abdb13093
Anton Davydov <antondavydov.o@gmail.com>
2018-03-16 00:20:22 +0300

    Use slug in deck show operation

--- spec/decks/operations/show_spec.rb
+++ spec/decks/operations/show_spec.rb
@@ -2,19 +2,19 @@
 
 RSpec.describe Decks::Operations::Show do
   let(:operation) { described_class.new(deck: deck_repo) }
-  let(:id) { 1 }
+  let(:slug) { '1234567' }
 
-  subject { operation.call(id) }
+  subject { operation.call(slug) }
 
   context 'when deck exist' do
-    let(:deck_repo) { double(:deck_repo, find_with_repos: Deck.new(id: 1)) }
+    let(:deck_repo) { double(:deck_repo, find_by_slug_with_repos: Deck.new(slug: '1234567')) }
 
     it { expect(subject).to be_right }
-    it { expect(subject.value).to eq Deck.new(id: 1) }
+    it { expect(subject.value.slug).to eq slug }
   end
 
   context 'when deck unexist' do
-    let(:deck_repo) { double(:deck_repo, find_with_repos: nil) }
+    let(:deck_repo) { double(:deck_repo, find_by_slug_with_repos: nil) }
 
     it { expect(subject).to be_left }
     it { expect(subject.value).to eq :not_found }
