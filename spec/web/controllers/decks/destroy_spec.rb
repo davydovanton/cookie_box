64eafbb0798b52c9fca833a502cc9af467018507
Anton Davydov <antondavydov.o@gmail.com>
2018-03-16 00:40:56 +0300

    Fix destroy deck controller payload

--- spec/web/controllers/decks/destroy_spec.rb
+++ spec/web/controllers/decks/destroy_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Web::Controllers::Decks::Destroy, type: :action do
 
   let(:mock_operation) { Mock::SuccessListOperation.new }
   let(:action) { described_class.new(operation: mock_operation) }
-  let(:params)  { { 'rack.session' => session, id: 1 } }
+  let(:params)  { { 'rack.session' => session, id: '123' } }
 
   context 'when account login' do
     let(:session) { { account: Account.new(id: 1) } }
@@ -13,7 +13,7 @@ RSpec.describe Web::Controllers::Decks::Destroy, type: :action do
     it { expect(action.call(params)).to redirect_to('/decks') }
 
     it 'calls operation with current account id' do
-      expect(mock_operation).to receive(:call).with(deck_id: 1, account_id: 1)
+      expect(mock_operation).to receive(:call).with(deck_slug: '123', account_id: 1)
       action.call(params)
     end
   end
