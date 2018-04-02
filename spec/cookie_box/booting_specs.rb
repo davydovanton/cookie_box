9a0c229d7c0b75d91f8b5f10a96178d2db1a7bf4
Anton Davydov <antondavydov.o@gmail.com>
2018-04-02 12:43:50 +0300

    Add more booting specs

--- spec/cookie_box/booting_specs.rb
+++ spec/cookie_box/booting_specs.rb
@@ -4,4 +4,8 @@ RSpec.describe 'Booting system' do
   it 'loads all dependencies to container' do
     expect { Container.keys.map { |key| Container[key] } }.to_not raise_error
   end
+
+  it 'loads domain dependencies right' do
+    expect(Container['decks.operations.all_issues']).to be_a Decks::Operations::AllIssues
+  end
 end
