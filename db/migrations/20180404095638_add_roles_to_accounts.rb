cd344b3655dc8ed9bdbae7c84940a968c18b1e7c
Anton Davydov <antondavydov.o@gmail.com>
2018-04-04 13:07:29 +0300

    Add roles field for account

---
+++ db/migrations/20180404095638_add_roles_to_accounts.rb
@@ -0,0 +1,11 @@
+Hanami::Model.migration do
+  change do
+    extension :pg_enum
+
+    create_enum(:account_roles, %w[regular admin])
+
+    alter_table :accounts do
+      add_column :roles, 'account_roles', null: false, default: 'regular'
+    end
+  end
+end
