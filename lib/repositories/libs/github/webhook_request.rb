f01aac4ce3ade5932c0e84b62193a5bb4ca286f1
Anton Davydov <antondavydov.o@gmail.com>
2018-04-08 22:07:55 +0300

    Create webhook for user repository

--- lib/repositories/libs/github/webhook_request.rb
+++ lib/repositories/libs/github/webhook_request.rb
@@ -7,9 +7,29 @@ module Repositories
     module Github
       class WebhookRequest
         include Dry::Monads::Either::Mixin
+        include Import['core.http_request']
 
-        def call(payload)
-          Right(:ok)
+        GITHUB_WEBHOOK_API_URL = 'https://api.github.com/repos/%{full_name}/hooks?access_token=%{token}'
+
+        NEW_HOOK_PAYLOAD = {
+          name: 'web',
+          active: true,
+          events: ['issues'],
+          config: {
+            url: 'http://cookie-box.cluster.davydovanton.com/webhooks/github',
+            content_type: 'json'
+          }
+        }
+
+        def call(token:, repository:)
+          response = http_request.post(
+            format(GITHUB_WEBHOOK_API_URL, full_name: repository.full_name, token: token),
+            NEW_HOOK_PAYLOAD
+          )
+
+          return Left(response.body) unless response.is_a?(Net::HTTPSuccess)
+
+          Right(status: :ok)
         end
       end
     end
