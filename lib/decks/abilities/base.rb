f2685db6126589c8bb02bba1f90a1553098e520a
Anton Davydov <antondavydov.o@gmail.com>
2018-05-05 21:14:07 +0300

    Create a ability check for create a decks for regular accounts

--- lib/decks/abilities/base.rb
+++ lib/decks/abilities/base.rb
@@ -5,8 +5,18 @@ module Decks
     class Base
       include Kan::Abilities
 
+      role(:regular) do |account, _|
+        account.roles.nil? || account.roles == 'regular'
+      end
+
+      ALLOWED_DECK_COUNT = 3
+
       register 'read' do |account, deck|
         deck&.published || (deck && deck.account_id == account&.id)
+      end
+
+      register 'create' do |account, deck_count|
+        deck_count >= 0 && deck_count <= ALLOWED_DECK_COUNT
       end
     end
   end
--- spec/decks/abilities/base_spec.rb
+++ spec/decks/abilities/base_spec.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe Decks::Abilities::Base do
+  let(:account) { Account.new(roles: 'regular') }
   let(:abilities) { Decks::Abilities::Base.new }
-  let(:deck) { Deck.new }
-  let(:account) { Account.new }
 
-  describe 'edit ability' do
+  describe 'read ability' do
+    let(:deck) { Deck.new }
+    let(:account) { Account.new(roles: 'regular') }
     let(:ability) { abilities.ability(:read) }
 
     subject { ability.call(account, deck) }
@@ -19,14 +20,14 @@ RSpec.describe Decks::Abilities::Base do
     context 'when deck publish is nil' do
       context 'and account is owner of deck' do
         let(:deck) { Deck.new(publish: false, account_id: 1) }
-        let(:account) { Account.new(id: 1) }
+        let(:account) { Account.new(id: 1, roles: 'regular') }
 
         it { expect(subject).to eq true }
       end
 
       context 'and account is not owner of deck' do
         let(:deck) { Deck.new(publish: false, account_id: 2) }
-        let(:account) { Account.new(id: 1) }
+        let(:account) { Account.new(id: 1, roles: 'regular') }
 
         it { expect(subject).to eq false }
       end
@@ -36,6 +37,43 @@ RSpec.describe Decks::Abilities::Base do
       let(:deck) { nil }
 
       it { expect(subject).to eq nil }
+    end
+  end
+
+  describe 'create ability' do
+    let(:deck_count) { 0 }
+    let(:ability) { abilities.ability(:create) }
+
+    subject { ability.call(account, deck_count) }
+
+    context 'when account has less than 0 decks' do
+      let(:deck_count) { -1 }
+
+      it { expect(subject).to eq false }
+    end
+
+    context 'when account has 0 decks' do
+      let(:deck_count) { 0 }
+
+      it { expect(subject).to eq true }
+    end
+
+    context 'when account has less than 3 decks' do
+      let(:deck_count) { 2 }
+
+      it { expect(subject).to eq true }
+    end
+
+    context 'when account has 3 decks' do
+      let(:deck_count) { 3 }
+
+      it { expect(subject).to eq true }
+    end
+
+    context 'when account has more than 3 decks' do
+      let(:deck_count) { 4 }
+
+      it { expect(subject).to eq false }
     end
   end
 end
