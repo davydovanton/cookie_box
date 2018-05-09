e614b22fb7ad81f30aa1fdf236e4e137fbaa87b8
Anton Davydov <antondavydov.o@gmail.com>
2018-05-09 16:58:03 +0300

    Complete github webhook loader and test it manualy

--- lib/cookie_box/repositories/account_repository.rb
+++ lib/cookie_box/repositories/account_repository.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class AccountRepository < Hanami::Repository
+  relations :accounts, :decks, :deck_repos, :repositories
+
   associations do
     has_many :decks
   end
 
   def find_by_uid(uid)
     root.where(uid: uid).map_to(Account).one
+  end
+
+  # TODO: SPECS
+  def owner_for_repository(repository_id)
+    root
+      .join(:decks)
+      .join(:deck_repos, deck_id: :id)
+      .where(deck_repos[:repository_id].qualified => repository_id)
+      .limit(1).map_to(Account).one
   end
 end
--- lib/repositories/operations/create_webhook.rb
+++ lib/repositories/operations/create_webhook.rb
@@ -4,10 +4,13 @@ module Repositories
   module Operations
     class CreateWebhook < Core::Operation
       include Dry::Monads::Do.for(:call)
+      include Dry::Monads::Maybe::Mixin
 
       include Import[
         'repositories.libs.github.webhook_request',
-        'repositories.libs.prersist_webhook_status'
+        'repositories.libs.prersist_webhook_status',
+        account_repo: 'repositories.account',
+        repository_repo: 'repositories.repository'
       ]
 
       VALIDATOR = Dry::Validation.Form do
@@ -16,8 +19,11 @@ module Repositories
 
       def call(payload)
         payload = yield VALIDATOR.call(payload).to_either
-        yield webhook_request.call(payload)
-        prersist_webhook_status.call(payload)
+        account = yield Maybe(account_repo.owner_for_repository(payload[:repository_id]))
+        repository = yield Maybe(repository_repo.find(payload[:repository_id]))
+
+        yield webhook_request.call(token: account.token, repository: repository)
+        prersist_webhook_status.call(webhook_owner_id: account.id, **payload)
         # notify
       end
     end
