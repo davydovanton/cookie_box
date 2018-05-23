36d2a30e94a5e1936427326687c5ffe5b4eaecaf
Anton Davydov <antondavydov.o@gmail.com>
2018-05-23 18:45:31 +0300

    Use repositories scope for abilities

--- system/boot/abilities.rb
+++ system/boot/abilities.rb
@@ -2,7 +2,8 @@
 
 Container.finalize(:abilities) do |container|
   abilities = Kan::Application.new(
-    deck: [Decks::Abilities::Anonymous.new, Decks::Abilities::Regular.new, Decks::Abilities::Admin.new]
+    deck: [Decks::Abilities::Anonymous.new, Decks::Abilities::Regular.new, Decks::Abilities::Admin.new],
+    repositories: [Repositories::Abilities::Regular.new, Repositories::Abilities::Admin.new]
   )
 
   container.register(:abilities, abilities)
